3e06914656a0fd00d195485e9f78a634ae0d9895
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-18 23:57:37 +0200

    Add makeGrid function

--- designs.js
+++ designs.js
@@ -5,6 +5,27 @@
 
 function makeGrid() {
 
-// Your code goes here!
+  const grid = document.getElementById("pixel_canvas");
 
-}
+  document.getElementById("submit").addEventListener("click", function() {
+
+    event.preventDefault();
+    grid.innerHTML = "";
+
+    const width = document.getElementById("input_width").value;
+    const height = document.getElementById("input_height").value;
+
+    for (let i = 0; i < height; i++) {
+      const rows = document.createElement("tr");
+      let j = 0;
+      while (j < width) {
+        const cells = document.createElement("td");
+        j++;
+        rows.appendChild(cells);
+      }
+      grid.appendChild(rows);
+    }
+  });
+};
+
+makeGrid();
